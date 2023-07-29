fd61003fdcbf5d17648c10d9ccb3935cfa2c9e8d
Smagic Jones <>
2023-07-30 00:06:42 +0100

    new slides for NCnews

--- src/data/ncnewsslides.js
+++ src/data/ncnewsslides.js
@@ -1,7 +1,9 @@
-import home from '../assets/NCNews/home.png';
-import moreInfo from '../assets/NCNews/moreInfo.png'
-import comment from '../assets/NCNews/comment.png'
+import one from "../assets/NCNews/1.png";
+import two from "../assets/NCNews/2.png";
+import three from "../assets/NCNews/3.png";
+import four from "../assets/NCNews/4.png";
+import five from "../assets/NCNews/5.png";
+import six from "../assets/NCNews/6.png";
+import seven from "../assets/NCNews/7.png";
 
-export const ncNewsSlides =  [
-    home, moreInfo, comment
-]
+export const ncNewsSlides = [one, two, three, four, five, six, seven];
